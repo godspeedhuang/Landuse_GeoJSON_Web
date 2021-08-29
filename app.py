4fc1c8a7f943b231226d9794db9d622571d89790
SyuanBo <godspeed88168@gmail.com>
2021-08-29 23:24:45 +0800

    third modify

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
 def search():
     # district_num = request.args.get('distrct', '')
     area_num = request.args.get('area', '')
-    return redirect('/'+area_num+'.geojson')
+    return redirect('S0101.geojson')
 
 
 app.run(port=3000)
