5294a87a727eba2d300f87167aa25a1190cf35db
SyuanBo <godspeed88168@gmail.com>
2021-08-31 16:34:17 +0800

    version2

--- sectors.py
+++ sectors.py
@@ -1,11 +1,12 @@
 import json
 
-with open('TXG.json', 'r', encoding='utf-8') as file:
+with open('PIF.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 
 data_1 = []
 for i in data:
+    data_1.append(i['計畫區代碼'])
     data_1.append(i['計畫區名稱'])
 
 print(data_1)
