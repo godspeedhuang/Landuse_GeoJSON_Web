ed28d928d10eb1c1b2cce0c470f8e44dd3df7eb8
SyuanBo <godspeed88168@gmail.com>
2021-09-06 20:22:41 +0800

    v

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask import send_file
 
 app = Flask(__name__, static_folder='static', static_url_path='/')
 
@@ -13,7 +14,7 @@ def index():
 
 
 @app.route('/search')
-def search():
+def download_file():
     district_code = request.args.get('district', '')
     return redirect(district_code+'.json')
 
